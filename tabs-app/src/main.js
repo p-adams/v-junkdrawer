933d646a414160ddd168cb5625f2b7fdda671cf1
Paul Adams <ghusiyy@gmail.com>
2021-11-14 22:43:07 -0800

    hardcode tabs for prototyping

--- tabs-app/src/main.js
+++ tabs-app/src/main.js
@@ -1,8 +1,21 @@
 import "../style.css";
 const template = document.createElement("template");
 template.innerHTML = `
-  <div>
-    testing
+<style>
+  .tab-container {
+    display: flex;
+    justify-content: space-evenly;
+    
+  }
+  .tab-container .tab.active {
+    border-bottom: 1px solid blue;
+  }
+ }
+ </style>
+  <div class="tab-container">
+    <div class="tab active">tab a</div>
+    <div class="tab">tab b</div>
+    <div class="tab">tab c</div>
   </div>`;
 customElements.define(
   "tabs-app",
