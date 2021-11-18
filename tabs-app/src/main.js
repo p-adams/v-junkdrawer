ec0b6d687454eaad54e6a95c5651b3294ff74732
Paul Adams <ghusiyy@gmail.com>
2021-11-17 22:40:30 -0800

    create tabs, tab content, and attach tab click handlers in class methods

--- tabs-app/src/main.js
+++ tabs-app/src/main.js
@@ -40,44 +40,51 @@ customElements.define(
       super();
       this.attachShadow({ mode: "open" });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
-      const tabContainer = this.shadowRoot.querySelector(".tab-container");
-      const tabContentContainer = this.shadowRoot.querySelector(
+      this.tabContainer = this.shadowRoot.querySelector(".tab-container");
+      this.tabContentContainer = this.shadowRoot.querySelector(
         ".tab-content-container"
       );
       this.tabs = JSON.parse(this.getAttribute("tabs"));
-      // create tabs and tab content
+      // create tabs, tab content, and attach tab click handlers
       for (let i = 0; i < this.tabs.length; i++) {
-        const newTab = document.createElement("div");
-        const newTabContent = document.createElement("slot");
-        newTabContent.setAttribute("name", this.tabs[i].label);
-        newTab.textContent = this.tabs[i].label;
-        newTab.classList.add("tab");
-        newTabContent.classList.add("tab-content");
-        if (i === 0) {
-          newTab.classList.add("active");
-          newTabContent.classList.add("active");
-        }
-        tabContainer.appendChild(newTab);
-        tabContentContainer.appendChild(newTabContent);
+        this.renderTabs(i);
+        this.renderTabContent(i);
+        this.attachTabActiveHandlers(i);
       }
-
-      const children = tabContainer.children;
-
-      for (let idx = 0; idx < children.length; idx++) {
-        const currentTab = children[idx];
-        currentTab.addEventListener("click", () => {
-          this.shadowRoot
-            .querySelectorAll(".tab")
-            .forEach((tab) => tab.classList.remove("active"));
-          this.shadowRoot
-            .querySelectorAll(".tab-content")
-            .forEach((tab) => tab.classList.remove("active"));
-          currentTab.classList.add("active");
-          this.shadowRoot
-            .querySelectorAll(".tab-content")
-            [idx].classList.add("active");
-        });
+    }
+    renderTabs(i) {
+      const newTab = document.createElement("div");
+      newTab.textContent = this.tabs[i].label;
+      newTab.classList.add("tab");
+      if (i === 0) {
+        newTab.classList.add("active");
       }
+      this.tabContainer.appendChild(newTab);
+    }
+    renderTabContent(i) {
+      const newTabContent = document.createElement("slot");
+      newTabContent.setAttribute("name", this.tabs[i].label);
+
+      newTabContent.classList.add("tab-content");
+      if (i === 0) {
+        newTabContent.classList.add("active");
+      }
+      this.tabContentContainer.appendChild(newTabContent);
+    }
+    attachTabActiveHandlers(i) {
+      const currentTab = this.tabContainer.children[i];
+      currentTab.addEventListener("click", () => {
+        this.shadowRoot
+          .querySelectorAll(".tab")
+          .forEach((tab) => tab.classList.remove("active"));
+        this.shadowRoot
+          .querySelectorAll(".tab-content")
+          .forEach((tab) => tab.classList.remove("active"));
+        currentTab.classList.add("active");
+        this.shadowRoot
+          .querySelectorAll(".tab-content")
+          [i].classList.add("active");
+      });
     }
   }
 );
