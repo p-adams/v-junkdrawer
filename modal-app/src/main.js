372dc9e78edece7dbf4f8b9096b8664ab0d3568b
p-adams <ghusiyy@gmail.com>
2021-11-22 21:28:34 -0800

    add basic markup and style for modal component

--- modal-app/src/main.js
+++ modal-app/src/main.js
@@ -1,9 +1,40 @@
 import "../style.css";
 const template = document.createElement("template");
 template.innerHTML = `
-<style></style>
-<div class="modal-container">
-modal
+<style>
+  .modal {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: black;
+    opacity: 0.4;
+  }
+  .content-wrapper {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    background-color: white;
+    margin: 15% auto;
+    width: 75%;
+  }
+  .dismiss {
+    font-size: 24px;
+    display: grid;
+    place-items: end;
+   
+  }
+</style>
+<div class="modal">
+  <div class="content-wrapper">
+  <div class="dismiss">&times;</div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Sodales ut etiam sit amet nisl purus in. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis.
+  Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+  </p>
+  </div>
 </div>
 `;
 customElements.define(
