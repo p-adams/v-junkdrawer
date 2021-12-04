7e6be43e97ad8909fdfc2b9a08ef39fab50a43a7
p-adams <ghusiyy@gmail.com>
2021-12-03 22:18:44 -0800

    impl for simple modal

--- src/components/modal/v-modal-consumer.vue
+++ src/components/modal/v-modal-consumer.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { ref } from "@vue/reactivity";
 import VModal from "../modal/v-modal.vue";
+const isModalOpen = ref(false);
 </script>
 <template>
-  <button>Open Modal</button>
-  <VModal :isOpen="true" :mask="false" />
+  <button @click="isModalOpen = !isModalOpen">Open Modal</button>
+  <VModal
+    :isOpen="isModalOpen"
+    :mask="true"
+    @close-modal="isModalOpen = false"
+  />
 </template>
 <style lang="scss"></style>
--- src/components/modal/v-modal.vue
+++ src/components/modal/v-modal.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
-const props = defineProps<{ mask: boolean; isOpen: boolean }>();
-console.log(props);
+defineProps<{ mask: boolean; isOpen: boolean }>();
 </script>
 
 <template>
-  <div :class="['modal', { mask: mask }]">Modal App</div>
+  <div :class="['modal', { isOpen, mask }]">
+    <div class="modal-container">
+      <div class="dismiss" @click="$emit('close-modal')">&times;</div>
+      <h3>modal content</h3>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  &.isOpen {
+    display: block;
+    &.mask {
+      background-color: black;
+      opacity: 0.8;
+    }
+    .modal-container {
+      display: flex;
+      flex-direction: column;
+      padding: 10px;
+      background-color: white;
+      margin: 15% auto;
+      width: 75%;
+      .dismiss {
+        display: grid;
+        place-items: end;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
