79f2043d7989683ca46c3513db2a2678da51ee74
p-adams <ghusiyy@gmail.com>
2021-12-19 23:37:46 -0800

    init action button component

--- src/components/button/v-action-button.vue
+++ src/components/button/v-action-button.vue
@@ -1,5 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const expand = ref(false);
+</script>
 <template>
-  <button class="action-button">Action</button>
+  <div class="action-button">
+    <div class="wrapper" @click="expand = !expand">
+      <span class="label">Actions</span>
+      <ul v-if="expand">
+        <li>create</li>
+        <li>read</li>
+        <li>update</li>
+        <li>delete</li>
+      </ul>
+    </div>
+  </div>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+.action-button {
+  .wrapper {
+    display: inline-flex;
+    flex-direction: column;
+    border: 1px solid lightgrey;
+    border-radius: 4px;
+    cursor: pointer;
+    background-color: #2196f3;
+    .label {
+      text-transform: uppercase;
+      color: white;
+      font-weight: 700;
+      padding: 10px;
+      &::after {
+        content: ">";
+        margin-left: 10px;
+        vertical-align: middle;
+      }
+    }
+    ul {
+      padding: 10px;
+      text-align: right;
+      background-color: white;
+      li {
+        margin-bottom: 10px;
+        &:hover {
+          background-color: lightgray;
+        }
+      }
+    }
+  }
+}
+</style>
