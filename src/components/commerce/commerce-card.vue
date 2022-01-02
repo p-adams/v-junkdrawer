b637df25cbd01674827b83caa8f59c0d904dc952
p-adams <ghusiyy@gmail.com>
2022-01-01 22:35:45 -0800

    add shimmer background to card image container

--- src/components/commerce/commerce-card.vue
+++ src/components/commerce/commerce-card.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 <template>
   <div class="commerce-card">
-    <div class="image-container"><slot name="image"> </slot></div>
+    <div class="image-container background"><slot name="image"> </slot></div>
     <div class="content-container">
       <slot name="content"> </slot>
     </div>
@@ -17,9 +17,27 @@
   border: 1px solid lightgray;
   padding: 6px;
   .image-container {
-    outline: 1px solid gray;
     height: 65%;
+    &.background {
+      animation: shimmer 2s infinite;
+      background: linear-gradient(
+        to right,
+        #eff1f3 4%,
+        #e2e2e2 25%,
+        #eff1f3 36%
+      );
+      background-size: 1000px 100%;
+    }
   }
+  @keyframes shimmer {
+    0% {
+      background-position: -1000px 0;
+    }
+    100% {
+      background-position: 1000px 0;
+    }
+  }
+
   .content-container {
     margin: 6px 0 6px 0;
   }
