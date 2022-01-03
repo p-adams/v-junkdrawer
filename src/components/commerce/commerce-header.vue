def33b9ddd7119c1dd8ea4131b0fac823cb9d99d
p-adams <ghusiyy@gmail.com>
2022-01-02 21:13:28 -0800

    init commerce header

---
+++ src/components/commerce/commerce-header.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{ pageTitle: string }>();
+</script>
+<template>
+  <header>
+    <div class="navbar">
+      <div class="navbar-items">
+        <span>{{ pageTitle }}</span>
+        <input />
+        <div>Account</div>
+        <div>cart</div>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>Books</li>
+        <li>Computers</li>
+        <li>Shoes</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  .navbar {
+    border-bottom: 1px solid lightgray;
+    margin: 10px 0 10px 0;
+    .navbar-items {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 8px;
+    }
+  }
+  nav {
+    ul {
+      list-style: none;
+      display: flex;
+      justify-content: space-evenly;
+    }
+  }
+}
+</style>
--- src/components/commerce/v-storefront.vue
+++ src/components/commerce/v-storefront.vue
@@ -1,3 +1,9 @@
-<script lang="ts" setup></script>
-<template><div>storefront</div></template>
+<script lang="ts" setup>
+import commerceHeader from "./commerce-header.vue";
+</script>
+<template>
+  <div>
+    <commerce-header page-title="storefront" />
+  </div>
+</template>
 <style></style>
