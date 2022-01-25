c93ee09f62a13f0e41e11917d55fa0c0c1ee2b25
p-adams <ghusiyy@gmail.com>
2022-01-24 23:49:07 -0800

    init order summary component

--- src/components/commerce/v-order-summary.vue
+++ src/components/commerce/v-order-summary.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <div>order summary</div>
+  <article class="order-summary-container">
+    <header><v-slot name="header" /></header>
+    <section><v-slot name="section" /></section>
+    <footer><v-slot name="footer" /></footer>
+  </article>
 </template>
+<style lang="scss" scoped>
+.order-summary-container {
+  border: 1px solid lightgray;
+}
+</style>
