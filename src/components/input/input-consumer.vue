f8fd381eb9255d0c28700b4b8b39b3e343c11e38
Paul Adams <ghusiyy@gmail.com>
2022-03-31 22:29:33 -0700

    sync text field value with e target value

--- src/components/input/input-consumer.vue
+++ src/components/input/input-consumer.vue
@@ -1,16 +1,25 @@
 <script lang="ts" setup>
+import { ref } from "vue";
 import VTextField from "./v-text-field.vue";
-function log(e: any) {
-  console.log(e.target);
-}
+const textFieldValue = ref<string>("");
 </script>
 <template>
   <article>
     <section>
       <h3>Text Field</h3>
-      <div>
-        <v-text-field @on-input="(e) => log(e)" />
+      <div class="text-field-wrapper">
+        <div>Output: {{ textFieldValue }}</div>
+        <v-text-field
+          placeholder="type something..."
+          @on-input="({ target }) => (textFieldValue = target.value)"
+        />
       </div>
     </section>
   </article>
 </template>
+<style lang="scss" scoped>
+.text-field-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
