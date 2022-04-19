37818016c0a7fc088893715b71dd723b3bc6eed7
Paul Adams <ghusiyy@gmail.com>
2022-04-18 23:16:34 -0700

    init image slider component

--- src/components/image/image-consumer.vue
+++ src/components/image/image-consumer.vue
@@ -1,9 +1,26 @@
-<script setup></script>
+<script setup>
+import ImageSlider from "./image-slider.vue";
+</script>
 <template>
   <article class="image-consumer-wrapper">
     <section>
       <h1>image consumer</h1>
+      <div class="image-component">
+        <h4>image slider</h4>
+        <image-slider />
+      </div>
     </section>
   </article>
 </template>
-<style scoped></style>
+<style scoped>
+.image-consumer-wrapper {
+  height: calc(100vh - 50px);
+  border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+}
+.image-component {
+  margin: 20px 0 20px 0;
+}
+</style>
---
+++ src/components/image/image-slider.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <div class="image-slider-wrapper">
+    <img src="" alt="image" />
+  </div>
+</template>
+<style scoped>
+.image-slider-wrapper {
+  height: 350px;
+}
+</style>
